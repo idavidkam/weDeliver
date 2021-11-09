182399613415510b2c194414502d3f7e792a7dd9
inonbezalel <inonbezalel@gmail.com>
2021-11-09 18:18:36 +0200

    add flash screen

--- app/src/main/java/app/shipcalc/MainActivity.kt
+++ app/src/main/java/app/shipcalc/MainActivity.kt
@@ -1,12 +1,22 @@
 package app.shipcalc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity,HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        },2000)
+
         // test
     }
 }
