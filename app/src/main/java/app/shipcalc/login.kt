3d8f480f717d5fc0482cfdbde600885d9c9c24fb
idavidkam <idavidkam@outlook.com>
2021-11-14 10:07:16 +0200

    add signin page

--- app/src/main/java/app/shipcalc/login.kt
+++ app/src/main/java/app/shipcalc/login.kt
@@ -1,12 +1,27 @@
 package app.shipcalc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.Button
 
 class login : AppCompatActivity() {
+    lateinit var createAccountButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         supportActionBar?.hide()
+
+        //move to next page - create account
+        createAccountButton = findViewById<Button>(R.id.create_account_Button)
+        createAccountButton.setOnClickListener{
+            val intent = Intent(this@login, signin::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
