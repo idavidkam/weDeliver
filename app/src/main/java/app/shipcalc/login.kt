9719f1b429be31bc6068db48346269d87645f651
idavidkam <idavidkam@outlook.com>
2021-11-14 13:23:23 +0200

    just update

--- app/src/main/java/app/shipcalc/login.kt
+++ app/src/main/java/app/shipcalc/login.kt
@@ -9,11 +9,20 @@ import android.widget.Button
 
 class login : AppCompatActivity() {
     lateinit var createAccountButton: Button
+    lateinit var loginButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         supportActionBar?.hide()
+
+        //move to next page - homepage
+        loginButton = findViewById(R.id.login_button)
+        loginButton.setOnClickListener{
+            val intent = Intent(this@login, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
 
         //move to next page - create account
         createAccountButton = findViewById<Button>(R.id.create_account_Button)
