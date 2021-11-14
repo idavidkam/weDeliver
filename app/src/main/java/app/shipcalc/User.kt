80e5d6ce8f5b98d3142468fc670a17bc9f89852e
inonbezalel <inonbezalel@gmail.com>
2021-11-14 18:01:31 +0200

    User updates

--- app/src/main/java/app/shipcalc/User.kt
+++ app/src/main/java/app/shipcalc/User.kt
@@ -9,44 +9,33 @@ class User(
 ) {
     // Need to check how to make it constant
     // Fields with getters and setters
-    private var id: Int = 0
+    private var id: Int = Id
         get() = field
         private set(value) {
             field = value
         }
 
-    private var firstName: String = ""
+    private var firstName: String = FirstName
         get() = field
         private set(value) {
             field = value
         }
 
-    private var lastName: String = ""
+    private var lastName: String = LastName
         get() = field
         private set(value) {
             field = value
         }
 
-    private var phoneNumber: String = " "
+    private var phoneNumber: String = PhoneNumber
         get() = field
         private set(value) {
             field = value
         }
 
-    private var password: String = " "
+    private var password: String = Password
         get() = field
         private set(value) {
             field = value
         }
-
-    // Constructor
-    init {
-        id = Id
-        firstName = FirstName
-        lastName = LastName
-        phoneNumber = PhoneNumber
-        password = Password
-    }
-
-
 }
