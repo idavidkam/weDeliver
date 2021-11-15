e406fdbd7c99998c4dbfd9765913a9f4317dfc0f
inonbezalel <inonbezalel@gmail.com>
2021-11-15 10:07:47 +0200

    User updates 2

--- app/src/main/java/app/shipcalc/HomeActivity.kt
+++ app/src/main/java/app/shipcalc/HomeActivity.kt
@@ -2,10 +2,17 @@ package app.shipcalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var addPackageButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        addPackageButton = findViewById(R.id.home_button_addPackage)
+        addPackageButton.setOnClickListener {
+        }
     }
 }
