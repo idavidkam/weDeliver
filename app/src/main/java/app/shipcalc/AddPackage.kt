d463bc334605d8dc3c615be1e610f233cd939b56
idavidkam <idavidkam@outlook.com>
2021-11-23 16:56:22 +0200

    fix bugs 2

--- app/src/main/java/app/shipcalc/AddPackage.kt
+++ app/src/main/java/app/shipcalc/AddPackage.kt
@@ -4,34 +4,38 @@ import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.database.FirebaseDatabase
 import java.lang.Exception
-
-// List for dropdown menu of package types
-val TypesList = listOf("Envelop", "Small Package", "Large Package")
-
-// Variables for controls
-lateinit var deliveryType: AutoCompleteTextView
-lateinit var sendButten: Button
-lateinit var packageType: EditText
-lateinit var isFragileCB: CheckBox
-lateinit var pkgWeight: EditText
-//lateinit var lang: EditText
-//lateinit var lat: EditText
-//lateinit var pkgName: EditText
-//lateinit var pkgAddress: EditText
-
-// FireBase
-var databade = FirebaseDatabase.getInstance()
-var myRef = databade.getReference()
-
-// Counter for new package
-var counter: Int = 0
 
 /**
  * This class is about to add a new package for delivery
  */
 class AddPackage : AppCompatActivity() {
+
+    // List for dropdown menu of package types
+    val TypesList = listOf("Envelop", "Small Package", "Large Package")
+
+    // Variables for controls
+    lateinit var deliveryType: AutoCompleteTextView
+    lateinit var sendButten: Button
+
+    /*lateinit var isFragileCB: CheckBox
+    lateinit var packageType: EditText
+    lateinit var pkgWeight: EditText
+    lateinit var lang: EditText
+    lateinit var lat: EditText
+    lateinit var pkgName: EditText
+    lateinit var pkgAddress: EditText*/
+
+    // FireBase
+    var databade = FirebaseDatabase.getInstance()
+    var myRef = databade.getReference()
+
+    // Counter
+    var counter: Int = 0
+    var errorCounter: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_package)
@@ -42,36 +46,47 @@ class AddPackage : AppCompatActivity() {
         (deliveryType as? AutoCompleteTextView)?.setAdapter(adapter)
 
         //findView
-         sendButten = findViewById<Button>(R.id.addPkgButtonSend)
-         var packageType = findViewById<EditText>(R.id.addPkgTextPackageType)
-         isFragileCB = findViewById<CheckBox>(R.id.addPkgCBisFragile)
-         var pkgWeight = findViewById<EditText>(R.id.addPkgTextWeight)
-         var lang = findViewById<EditText>(R.id.addPkgTextLongitude)
-         var lat = findViewById<EditText>(R.id.addPkgTextLatitude)
-         var pkgName = findViewById<EditText>(R.id.addPkgTextAddresseeName)
-         var pkgAddress = findViewById<EditText>(R.id.addPkgTextAddresseeAddress)
+        sendButten = findViewById<Button>(R.id.addPkgButtonSend)
 
 
         sendButten.setOnClickListener {
             try {
+
+                // find view
+                var isFragileCB = findViewById<CheckBox>(R.id.addPkgCBisFragile).isChecked
+                //var packageType = findViewById<TextInputEditText>(R.id.addPkgTextPackageType)
+                var pkgWeight = findViewById<TextInputEditText>(R.id.addPkgTextWeight).text.toString()
+                var lang = findViewById<TextInputEditText>(R.id.addPkgTextLongitude).text.toString()
+                var lat = findViewById<TextInputEditText>(R.id.addPkgTextLatitude).text.toString()
+                var pkgName = findViewById<TextInputEditText>(R.id.addPkgTextAddresseeName).text.toString()
+                var pkgAddress = findViewById<TextInputEditText>(R.id.addPkgTextAddresseeAddress).text.toString()
+                
                 addPackage(
                     PackageDeliver(
-                        ConvertPackageTypeToEnum(packageType),
-                        isFragileCB.isChecked,
-                        pkgWeight.text.toString().toDouble(),
-                        Coordinate(lang.text.toString().toDouble(), lat.text.toString().toDouble()),
-                        pkgName.text.toString(),
-                        pkgAddress.text.toString()
-                    )
-                )
-            }
-            catch (E: Exception){
+                        PackageTypesEnum.ENVELOPE,
+                        isFragileCB,
+                        pkgWeight.toDouble(),
+                        Coordinate(lang.toDouble(), lat.toDouble()),
+                        pkgName,
+                        pkgAddress
+                    ))
+                Toast.makeText(this, "The package was sent successfully", Toast.LENGTH_SHORT).show()
+
+                //clear view
+                findViewById<TextInputEditText>(R.id.addPkgTextWeight).text?.clear()
+                findViewById<TextInputEditText>(R.id.addPkgTextLongitude).text?.clear()
+                findViewById<TextInputEditText>(R.id.addPkgTextLatitude).text?.clear()
+                findViewById<TextInputEditText>(R.id.addPkgTextAddresseeName).text?.clear()
+                findViewById<TextInputEditText>(R.id.addPkgTextAddresseeAddress).text?.clear()
+
+            } catch (E: Exception) {
                 val alertDialogBuilder = AlertDialog.Builder(this)
                 alertDialogBuilder.setTitle("ERROR")
-                    .setMessage("We apologize but an error occurred while trying to send the information")
+                    .setMessage("We apologize but an error occurred while trying to send the information\n\n" + E.toString())
                     .setIcon(R.drawable.ic_baseline_error_24)
                 val alertDialog = alertDialogBuilder.create()
                 alertDialog.show()
+                myRef.child("ERRORS").child("ERROR ${++errorCounter}").setValue(E.toString())
             }
         }
     }
