4248f4b42246c9465a32cb667c93c24c121f27fa
inonbezalel <inonbezalel@gmail.com>
2021-12-26 17:09:09 +0200

    add user firebase

--- app/src/main/java/app/shipcalc/Repository.kt
+++ app/src/main/java/app/shipcalc/Repository.kt
@@ -1,8 +1,18 @@
 package app.shipcalc
 
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+
 public class Repository {
 
+    var userCounter: Int = 0
+    // fire base
+    var databade = FirebaseDatabase.getInstance()
+    var myRef = databade.getReference()
+
     fun addUser(user : User){
+        var addRef = myRef.child("NewUser").child("User_${++userCounter}")
+        //addRef.child("userName").setValue(user.firstName.toString())
 
 
     }
